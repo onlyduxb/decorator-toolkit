d9d2f8471216f258d72801aaadc35c505cb088c2
Adam <adamworsnip@gmail.com>
2026-02-12 09:37:21 +0000

    Added a success message option to log

--- decorator_toolkit/decorator_tools.py
+++ decorator_toolkit/decorator_tools.py
@@ -10,7 +10,7 @@ P = ParamSpec("P")
 R = TypeVar("R")
 
 
-def log(msg: str | None = None) -> Callable[[Callable[P, R]], Callable[P, R]]:
+def log(msg: str | None = None, success_msg: str | None = None) -> Callable[[Callable[P, R]], Callable[P, R]]:
     """Log a function with the option for a custom message."""
 
     def decorator(func: Callable[P, R]) -> Callable[P, R]:
@@ -21,6 +21,8 @@ def log(msg: str | None = None) -> Callable[[Callable[P, R]], Callable[P, R]]:
             logging.debug(f"{func.__name__} returned {result} ({type(result)})")
             if msg is not None:
                 logging.info(msg)
+            if isinstance(result, bool) and result:
+                logging.info(success_msg)
             return result
 
         return wrapper
